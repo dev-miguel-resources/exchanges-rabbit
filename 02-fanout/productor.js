9b3747132da071f24abdc5015475d17cdf80728b
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-05-04 23:15:01 -0400

    fanout, direct and topic exchange examples

---
+++ 02-fanout/productor.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib');
+
+const args = process.argv.slice(2);
+
+(async () => {
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();  
+
+    const exchangeName = "exchange-fanout";
+    await channel.assertExchange(exchangeName, "fanout", { durable: true });
+
+    const message = args.length > 0 ? args[0] : "message default";
+
+    channel.publish(exchangeName, "", Buffer.from(message));
+
+    setTimeout(() => {
+        connection.close();
+        process.exit(0);
+    }, 4000);
+})();
---
+++ 04-topic/consumer.js
@@ -0,0 +1,25 @@
+const amqp = require('amqplib');
+
+const args = process.argv.slice(2);
+
+(async () => {
+  const connection = await amqp.connect('amqp://localhost')
+  const channel = await connection.createChannel()
+
+  const exchangeName = 'exchange-topic'
+  await channel.assertExchange(exchangeName, 'topic', { durable: true })
+
+  const assertQueue = await channel.assertQueue('', { exclusive: true })
+  const routingKey = args.length > 1 ? args[1] : "key-direct";
+
+  await channel.bindQueue(assertQueue.queue, exchangeName, routingKey);
+
+  const noAck = args.length > 0 ? true : false
+
+  channel.consume(
+    assertQueue.queue,
+    (message) => console.log(message.content.toString()),
+    { noAck },
+  )
+})();
+
